e51b488142c3d33f4ceb1c3ca7cb5dee0178b029
Jeff Weiss <jweiss@redhat.com>
2012-02-19 23:37:54 -0500

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,46 @@
+-- default desktop configuration for Fedora
+
+import System.Posix.Env (getEnv)
+import Data.Maybe (maybe)
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.Spiral
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+
+myLayoutHook =  Full ||| Mirror (spiral (6/7)) ||| mytall
+ where
+   mytall = Tall nmaster delta ratio
+   nmaster = 1
+   ratio = 1/2
+   delta = 1/100
+
+main = do
+     session <- getEnv "DESKTOP_SESSION"
+     xmproc <- spawnPipe "/usr/bin/xmobar /home/jweiss/.xmonad/.xmobarrc"
+     xmonad $ (maybe desktopConfig desktop session)
+             { modMask = mod4Mask
+              ,terminal = "gnome-terminal --hide-menubar" 
+              ,layoutHook = avoidStruts myLayoutHook
+              ,manageHook = manageDocks <+> manageHook defaultConfig
+              ,logHook = dynamicLogWithPP xmobarPP
+                         { ppOutput = hPutStrLn xmproc
+                          ,ppTitle = xmobarColor "green" "" . shorten 50
+                         }
+             }    
+             `additionalKeys`
+             [ ((mod4Mask, xK_u), spawn "exe=`dmenu_run | dmenu` && eval \"exec $exe\"")
+             ] 
+
+
+desktop "gnome" = gnomeConfig
+desktop "kde" = kde4Config
+desktop "xfce" = xfceConfig
+desktop _ = desktopConfig
