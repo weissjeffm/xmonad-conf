bfeece8ac86eaca54f3fc0b45faa443fe14db366
Jeff Weiss <jweiss@redhat.com>
2015-02-25 11:42:26 -0500

    More stuff

--- xmonad.hs
+++ xmonad.hs
@@ -6,46 +6,46 @@ import Data.Maybe (maybe)
 import XMonad
 import XMonad.Config.Desktop
 import XMonad.Config.Gnome
-import XMonad.Config.Kde
-import XMonad.Config.Xfce
+
+
 import XMonad.Util.EZConfig
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.SpawnOnce
 import XMonad.Layout.Spiral
 import XMonad.Util.Run(spawnPipe)
 import System.IO
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.SetWMName
+-- import XMonad.Layout.Rename
+import XMonad.Layout.NoBorders
 
-myLayoutHook =  Full ||| Mirror (spiral (6/7)) ||| mytall
+myLayoutHook =  Full ||| Mirror (spiral (6/7)) ||| mytall ||| Mirror (mytall)
  where
    mytall = Tall nmaster delta ratio
    nmaster = 1
    ratio = 1/2
-   delta = 1/100
+   delta = 2/100
 
 main = do
-     session <- getEnv "DESKTOP_SESSION"
      xmproc <- spawnPipe "/usr/bin/xmobar /home/jweiss/.xmonad/.xmobarrc"
-     xmonad $ (maybe desktopConfig desktop session)
+     xmonad (gnomeConfig
              { modMask = mod4Mask
               ,terminal = "gnome-terminal --hide-menubar" 
-              ,layoutHook = avoidStruts myLayoutHook
-              ,startupHook = setWMName "LG3D"
+              ,layoutHook = smartBorders (avoidStruts myLayoutHook)
+              ,startupHook = mystartuphook <+> startupHook gnomeConfig
               ,manageHook = manageDocks <+> manageHook defaultConfig
               ,handleEventHook = fullscreenEventHook
-              ,focusedBorderColor = "#00FF00"
+              ,focusedBorderColor = "#99FF99"
+              ,normalBorderColor = "#292929"
               ,logHook = dynamicLogWithPP xmobarPP
                          { ppOutput = hPutStrLn xmproc
                           ,ppTitle = xmobarColor "white" "" . shorten 50
                          }
              }    
-             `additionalKeys`
-             [ ((mod4Mask, xK_u), spawn "dmenu_run")
-             ] 
+             `additionalKeys` [ ((mod4Mask, xK_u), spawn "dmenu_run")] )
 
-
-desktop "gnome" = gnomeConfig
-desktop "kde" = kde4Config
-desktop "xfce" = xfceConfig
-desktop _ = desktopConfig
+mystartuphook = spawnOnce "xsetroot -gray" <+> 
+                spawnOnce "stalonetray -i 17 -s 17" <+>
+                spawnOnce "xscreensaver" <+>
+                spawnOnce "setxkbmap -option ctrl:nocaps"
