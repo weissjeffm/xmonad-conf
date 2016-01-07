92cc130db3075c61b21d1786467c1fc1af02ff2d
Jeff Weiss <me@jweiss.com>
2016-01-07 09:02:53 -0200

    Add mail check

--- xmonad.hs
+++ xmonad.hs
@@ -9,6 +9,7 @@ import XMonad.Config.Gnome
 
 
 import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.SpawnOnce
@@ -31,7 +32,7 @@ main = do
      xmproc <- spawnPipe "/usr/bin/xmobar /home/jweiss/.xmonad/.xmobarrc"
      xmonad (gnomeConfig
              { modMask = mod4Mask
-              ,terminal = "gnome-terminal --hide-menubar" 
+              ,terminal = "gnome-terminal --hide-menubar"
               ,layoutHook = smartBorders (avoidStruts myLayoutHook)
               ,startupHook = mystartuphook <+> startupHook gnomeConfig
               ,manageHook = manageDocks <+> manageHook defaultConfig
@@ -42,10 +43,24 @@ main = do
                          { ppOutput = hPutStrLn xmproc
                           ,ppTitle = xmobarColor "white" "" . shorten 50
                          }
-             }    
-             `additionalKeys` [ ((mod4Mask, xK_u), spawn "dmenu_run")] )
+             }
+             `additionalKeys`
+             [
+               ((mod4Mask, xK_u), spawn "dmenu_run")
+              ,((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; sleep 1; xset dpms force off")
+              ,((mod4Mask .|. shiftMask, xK_f), spawn "nautilus")
+              ,((mod4Mask .|. shiftMask, xK_s), spawn "gnome-screenshot -i")
+              ,((mod4Mask .|. shiftMask, xK_i), spawn "/home/jweiss/dock-in.sh")
+              ,((mod4Mask, xF86XK_MonBrightnessDown), spawn "xbacklight -10")
+              ,((mod4Mask, xF86XK_MonBrightnessUp), spawn "xbacklight +10")
+              ,((mod4Mask .|. shiftMask, xK_o), spawn "/home/jweiss/dock-out.sh")
+              ,((mod4Mask .|. shiftMask, xK_m), spawn "/home/jweiss/bin/mailrun.sh")
 
-mystartuphook = spawnOnce "xsetroot -gray" <+> 
+             ] )
+
+mystartuphook = setWMName "LG3D" <+>
+                spawnOnce "xsetroot -gray" <+>
                 spawnOnce "stalonetray -i 17 -s 17" <+>
                 spawnOnce "xscreensaver" <+>
-                spawnOnce "setxkbmap -option ctrl:nocaps"
+                spawnOnce "setxkbmap -option ctrl:nocaps" <+> -- capslock -> ctrl
+                spawnOnce "xset r rate 180 45" -- keyboard repeat rate
